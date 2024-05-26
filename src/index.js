1f63e4c2792662876cec7cefeb16f8df591ce706
ritesh0304 <ranaritesh0304@gmail.com>
2024-05-26 12:21:08 +0530

    mongoose setup

---
+++ src/index.js
@@ -0,0 +1,54 @@
+// require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv"
+
+import express from 'express'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const app=express();
+
+;(async ()=>{
+  try{
+     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+     app.on("error",(error)=>{
+        console.log("error");
+        throw error;
+     })
+
+     app.listen(proces.env.PORT,()=>{
+        console.log('app is listening on ', process.env.PORT)
+     })
+  }catch(error){
+    console.error("error: ",error)
+  }
+})()
+
+*/
